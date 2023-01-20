e3cf831b0337d52d0d8ce2b4c52320b5678fe611
Kainatkhan90 <kainatkhan123kainatkhan123@gmail.com>
2023-01-20 11:43:54 +0500

    Changed title name

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Firebase project',
       theme: ThemeData(
         primarySwatch: Colors.brown,
       ),
